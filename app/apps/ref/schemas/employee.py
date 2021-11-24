122348eb259c1b268004bd133ef698f614cfee6d
AnViSe <anvise@grodno.belpost.by>
2021-11-24 23:24:53 +0300

    Refactoring models & relations

--- app/apps/ref/schemas/employee.py
+++ app/apps/ref/schemas/employee.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+from apps.ref.schemas.person import Person
+
 
 class EmployeeBase(BaseModel):
     tab_num: int
@@ -10,10 +12,15 @@ class EmployeeBase(BaseModel):
 
 class Employee(EmployeeBase):
     id: int
+    person: Optional[Person] = None
 
     class Config:
         orm_mode = True
 
 
 class EmployeeCreate(EmployeeBase):
+    pass
+
+
+class EmployeeUpdate(EmployeeBase):
     pass
