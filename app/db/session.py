6c6837ee73182f5e863619f35ca285f6d751dd4e
AnViSe <anvise@grodno.belpost.by>
2021-11-30 23:29:03 +0300

    Dependency overrides

--- app/db/session.py
+++ app/db/session.py
@@ -15,3 +15,18 @@ EngineAsync = create_async_engine(settings.SQLALCHEMY_DATABASE_URL,
 SessionAsync = sessionmaker(EngineAsync,
                             class_=AsyncSession,
                             expire_on_commit=False)
+
+# https://t.me/fastapi_ru/30947
+# def session_getter(db_url: str, echo: bool):
+#     engine = create_async_engine(db_url, pool_pre_ping=True, echo=echo, future=True,
+#                                  connect_args={'check_same_thread': False})
+#     Session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+#
+#     async def get_db_session() -> AsyncSession:
+#         db = Session()
+#         try:
+#             yield db
+#         finally:
+#             await db.close()
+#
+#     return get_db_session
