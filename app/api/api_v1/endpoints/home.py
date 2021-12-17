af17591e7ca2ee3f8c66ab3aa132d3694d9405ba
AnViSe <anvise@grodno.belpost.by>
2021-12-17 17:31:14 +0300

    Test rate endpoint

--- app/api/api_v1/endpoints/home.py
+++ app/api/api_v1/endpoints/home.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from apps.bank.cruds.currency import import_rate_start_date
 from core.config import settings
 
 router = APIRouter(tags=['Home'])
@@ -8,3 +9,10 @@ router = APIRouter(tags=['Home'])
 @router.get('/')
 async def home():
     return {'Welcome to API': settings.PROJECT_NAME}
+
+
+@router.get('/rate/{date}')
+async def home(date: str):
+    # result = await import_rate_start_date(date)
+    result = await import_rate_start_date()
+    return result
